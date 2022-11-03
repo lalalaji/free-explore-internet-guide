f31cc55eae750baa6e9bca1b331d1b7455c2d735
lalalaji <95526077+lalalaji@users.noreply.github.com>
2022-11-04 02:26:08 +0800

    添加了搜索框

--- .vuepress/plugins.ts
+++ .vuepress/plugins.ts
@@ -1,4 +1,16 @@
 import { PluginConfig } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
 
 export const plugins: PluginConfig = [
+  searchPlugin({
+    locales: {
+      '/': {
+        placeholder: 'Search',
+      },
+      '/zh/': {
+        placeholder: '搜索',
+      },
+    },
+    isSearchable: (page) => page.path !== '/',
+  }),
 ]
